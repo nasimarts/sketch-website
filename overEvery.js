037796db35200efea5fafb45d184f3dd18d8c144
sketchart <soikofoflend@gmail.com>
2026-09-13 20:00:38 +0600

    Add files via upload

--- overEvery.js
+++ overEvery.js
@@ -1,34 +1,5 @@
-var arrayEvery = require('./_arrayEvery'),
-    createOver = require('./_createOver');
+var convert = require('./convert'),
+    func = convert('overEvery', require('../overEvery'));
 
-/**
- * Creates a function that checks if **all** of the `predicates` return
- * truthy when invoked with the arguments it receives.
- *
- * Following shorthands are possible for providing predicates.
- * Pass an `Object` and it will be used as an parameter for `_.matches` to create the predicate.
- * Pass an `Array` of parameters for `_.matchesProperty` and the predicate will be created using them.
- *
- * @static
- * @memberOf _
- * @since 4.0.0
- * @category Util
- * @param {...(Function|Function[])} [predicates=[_.identity]]
- *  The predicates to check.
- * @returns {Function} Returns the new function.
- * @example
- *
- * var func = _.overEvery([Boolean, isFinite]);
- *
- * func('1');
- * // => true
- *
- * func(null);
- * // => false
- *
- * func(NaN);
- * // => false
- */
-var overEvery = createOver(arrayEvery);
-
-module.exports = overEvery;
+func.placeholder = require('./placeholder');
+module.exports = func;
